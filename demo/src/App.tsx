ac7e68a0ad361fd7ff408fb591dda05d80f5d902
Dean Kerr <dkerr@scottlogic.co.uk>
2019-05-21 16:42:24 +0100

    Add maximized demo page

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,20 +1,27 @@
-import React from "react";
+import * as Prism from "prismjs";
+import React, {useEffect} from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import styles from "./App.module.css";
 import {ReactComponent as Logo} from "./images/logo-openfin.svg";
 import Home from "./pages/home";
+import Maximized from "./pages/maximized";
 
 const App: React.FC = () => {
+    useEffect(Prism.highlightAll, []);
+
     return (
         <div className={styles.containerApp}>
             <div className={styles.containerHeader}>
                 <Logo />
                 <div className={styles.subtitle}>React Hooks</div>
             </div>
-            <Router>
-                <Route exact path="/" component={Home} />
-            </Router>
+            <div className={styles.containerContent}>
+                <Router>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/demo/maximized" component={Maximized} />
+                </Router>
+            </div>
         </div>
     );
 };
---
+++ demo/src/pages/maximized/Maximized.tsx
@@ -0,0 +1,42 @@
+import {useMaximized} from "openfin-react-hooks";
+import React from "react";
+
+import styles from "./Maximized.module.css";
+
+const codeExample = `
+    import {useMaximized} from "openfin-react-hooks";
+
+    const Maximized: React.FC = () => {
+        const [maximized, setMaximized] = useMaximized();
+
+        return (
+            <div>
+                <div>Window is <strong>{maximized ? "maximized" : "not maximized"}</strong></div>
+                <button type="button" onClick={() => setMaximized(!maximized)}>Toggle Maximize</button>
+            </div>
+        )
+    }
+`;
+
+const Maximized: React.FC = () => {
+    const [maximized, setMaximized] = useMaximized();
+
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>useMaximize</h1>
+            <div className={styles.description}>Please maximize / unmaximize the window to demonstrate this hook</div>
+            <pre>
+                <code className="language-javascript">
+                    {codeExample}
+                </code>
+            </pre>
+            <h2>Example</h2>
+            <div>Window is <strong>{maximized ? "maximized" : "not maximized"}</strong></div>
+            <br />
+            <button type="button" onClick={() => setMaximized(!maximized)}>Toggle Maximize</button>
+
+        </div>
+    );
+};
+
+export default Maximized;

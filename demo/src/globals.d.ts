ade3a7a28926d6bb15e01569d7e3279d60f4445a
Dean Kerr <dkerr@scottlogic.co.uk>
2019-05-22 15:12:38 +0100

    Update global fin typing

--- demo/src/globals.d.ts
+++ demo/src/globals.d.ts
@@ -1,6 +1,5 @@
 // tslint:disable-next-line:interface-name
 interface Window {
-    fin: {
-        desktop: fin.OpenFinDesktop;
-    };
+    // https://devblogs.microsoft.com/typescript/announcing-typescript-2-9-2/#import-types
+    fin: import("openfin");
 }

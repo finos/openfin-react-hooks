aeabb05e2e0b1fcb839f041ba14600ac891dd7ee
Dean Kerr <dkerr@scottlogic.co.uk>
2019-05-31 17:17:09 +0100

    Add undocking transitions

--- src/useDockWindow.ts
+++ src/useDockWindow.ts
@@ -6,6 +6,7 @@ import {useEffect, useState} from "react";
 
 import {IDimensions} from "../index";
 import {ScreenEdge} from "./ScreenEdge";
+import usePreviousValue from "./utils/usePreviousValue";
 
 const ANIMATION_DURATION: number = 250;
 let isAnimating = false;
@@ -20,24 +21,54 @@ const getMonitorRect = async (bounds: Bounds): Promise<Rect> => {
         || monitorInfo.primaryMonitor.availableRect;
 };
 
-const getStretchTransition = (edge: ScreenEdge, screenBounds: Rect, stretchToFit: IDimensions): Transition => ({
+const getStretchedUndockTransition = (previousEdge: ScreenEdge, monitorBounds: Rect,
+                                      stretchToFit: IDimensions): Transition => ({
     position: {
         duration: ANIMATION_DURATION,
-        left: edge !== ScreenEdge.RIGHT ? screenBounds.left : screenBounds.right - stretchToFit.dockedWidth,
+        left: previousEdge !== ScreenEdge.RIGHT ?
+            25 : monitorBounds.right - stretchToFit.dockedWidth - 25,
         relative: false,
-        top: edge !== ScreenEdge.BOTTOM ? screenBounds.top : screenBounds.bottom - stretchToFit.dockedHeight,
+        top: previousEdge !== ScreenEdge.BOTTOM ? 25 : monitorBounds.bottom - stretchToFit.dockedHeight - 25,
+    },
+    size: {
+        duration: ANIMATION_DURATION,
+        height: previousEdge === ScreenEdge.LEFT || previousEdge === ScreenEdge.RIGHT ?
+            monitorBounds.bottom - monitorBounds.top - 50 : stretchToFit.dockedHeight,
+        relative: false,
+        width: previousEdge === ScreenEdge.TOP || previousEdge === ScreenEdge.BOTTOM ?
+            monitorBounds.right - monitorBounds.left - 50 : stretchToFit.dockedWidth,
+    },
+});
+
+const getUndockTransition = (previousEdge: ScreenEdge, windowBounds: Bounds): Transition => ({
+    position: {
+        duration: ANIMATION_DURATION,
+        left: previousEdge === ScreenEdge.LEFT ? 25 : previousEdge === ScreenEdge.RIGHT ?
+            windowBounds.left - 25 : windowBounds.left,
+        relative: false,
+        top: previousEdge === ScreenEdge.TOP ? 25 : previousEdge === ScreenEdge.BOTTOM ?
+            windowBounds.top - 25 : windowBounds.top,
+    },
+});
+
+const getStretchedDockTransition = (edge: ScreenEdge, monitorBounds: Rect, stretchToFit: IDimensions): Transition => ({
+    position: {
+        duration: ANIMATION_DURATION,
+        left: edge !== ScreenEdge.RIGHT ? monitorBounds.left : monitorBounds.right - stretchToFit.dockedWidth,
+        relative: false,
+        top: edge !== ScreenEdge.BOTTOM ? monitorBounds.top : monitorBounds.bottom - stretchToFit.dockedHeight,
     },
     size: {
         duration: ANIMATION_DURATION,
         height: edge === ScreenEdge.TOP || edge === ScreenEdge.BOTTOM ?
-            stretchToFit.dockedHeight : screenBounds.bottom - screenBounds.top,
+            stretchToFit.dockedHeight : monitorBounds.bottom - monitorBounds.top,
         relative: false,
         width: edge === ScreenEdge.TOP || edge === ScreenEdge.BOTTOM ?
-            screenBounds.right - screenBounds.left : stretchToFit.dockedWidth,
+            monitorBounds.right - monitorBounds.left : stretchToFit.dockedWidth,
     },
 });
 
-const getTransition = (edge: ScreenEdge, screenBounds: Rect, windowBounds: Bounds): Transition => ({
+const getDockTransition = (edge: ScreenEdge, screenBounds: Rect, windowBounds: Bounds): Transition => ({
     position: {
         duration: ANIMATION_DURATION,
         left: edge === ScreenEdge.LEFT ? screenBounds.left : edge === ScreenEdge.RIGHT ?
@@ -57,6 +88,7 @@ const getTransition = (edge: ScreenEdge, screenBounds: Rect, windowBounds: Bound
 export default (initialEdge = ScreenEdge.NONE, toMove: _Window = fin.Window.getCurrentSync(),
                 allowUserToUndock: boolean = true, stretchToFit?: IDimensions) => {
     const [edge, setEdge] = useState(initialEdge);
+    const previousEdge = usePreviousValue<ScreenEdge>(edge);
 
     useEffect(() => {
         const handleBoundsChanged = (event: fin.WindowBoundsEvent) => {
@@ -98,18 +130,26 @@ export default (initialEdge = ScreenEdge.NONE, toMove: _Window = fin.Window.getC
 
     useEffect(() => {
         const doWindowActions = async () => {
-            if (edge === ScreenEdge.NONE) {
-                return;
-            }
+            isAnimating = true; // set flag to prevent bounds listener from resetting edge to NONE
 
             const bounds: Bounds = await toMove.getBounds();
             const monitorRect: Rect = await getMonitorRect(bounds);
 
-            isAnimating = true; // set flag to prevent bounds listener from resetting edge to NONE
-            if (stretchToFit) {
-                await toMove.animate(getStretchTransition(edge, monitorRect, stretchToFit), {interrupt: true});
+            if (edge === ScreenEdge.NONE) {
+                if (stretchToFit) {
+                    await toMove.animate(getStretchedUndockTransition(previousEdge!, monitorRect, stretchToFit),
+                        {interrupt: true});
+                } else {
+                    await toMove.animate(getUndockTransition(previousEdge!, bounds), {interrupt: true});
+                }
+
             } else {
-                await toMove.animate(getTransition(edge, monitorRect, bounds), {interrupt: true});
+                if (stretchToFit) {
+                    await toMove.animate(getStretchedDockTransition(edge, monitorRect, stretchToFit),
+                        {interrupt: true});
+                } else {
+                    await toMove.animate(getDockTransition(edge, monitorRect, bounds), {interrupt: true});
+                }
             }
         };
 

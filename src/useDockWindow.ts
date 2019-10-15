4db77eedcdcd8ca9ca8b956219dcb195b5207c14
kangelSL <51117162+kangelSL@users.noreply.github.com>
2019-10-15 14:44:03 +0100

    Typescript generic

--- src/useDockWindow.ts
+++ src/useDockWindow.ts
@@ -9,7 +9,7 @@ import { ScreenEdge } from "./ScreenEdge";
 import transitions from "./useDockWindow.transitions";
 import usePreviousValue from "./utils/usePreviousValue";
 
-interface IBoundsChangedEvent extends fin.WindowBoundsEvent<"window", "bounds-changed"> {
+interface IBoundsChangedEvent extends fin.WindowBoundsEvent {
     changeType: number;
     reason?: "animation" | "self";
 }

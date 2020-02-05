f12ff52b20408385017b1765297fd66bfb4697ff
Matthew <masmith@scottlogic.co.uk>
2020-02-05 18:52:43 +0000

    Attempt to switch out hooks depending on runtime version. Not working

--- demo/src/pages/childWindow/components/DemoActions.tsx
+++ demo/src/pages/childWindow/components/DemoActions.tsx
@@ -1,5 +1,5 @@
-import { IUseChildWindowOptions, useChildWindow } from "openfin-react-hooks";
-import React from "react";
+import { IUseChildWindowOptions, useChildWindow, useChildWindowV1 } from "openfin-react-hooks";
+import React, { useState, useEffect } from "react";
 import ReactHtmlParser from "react-html-parser";
 
 interface IProps {
@@ -13,7 +13,24 @@ const DEFAULT_CHILD_BODY: JSX.Element = (
 );
 
 export default ({ CHILD_WINDOW_HOOK_OPTIONS, textAreaValue }: IProps) => {
-    const childWindow = useChildWindow(CHILD_WINDOW_HOOK_OPTIONS);
+    const childWindowV2 = useChildWindow(CHILD_WINDOW_HOOK_OPTIONS);
+    const childWindowV1 = useChildWindowV1(CHILD_WINDOW_HOOK_OPTIONS);
+    const [childWindow, setChildWindow] = useState<any>({ launch: ()=> {}, populate: (_1: any) => {}, close: () => {}, windowRef: false, state: "INITIAL"});
+    useEffect(() => {
+        console.log("useEffect");
+        fin.System.getVersion().then((fullVersion) => {
+            const versions = fullVersion.split(".");
+            const jsApiFullVersion = parseInt(versions[2]);
+            if (jsApiFullVersion < 39) {
+                console.log("V1");
+                setChildWindow(childWindowV1);
+            } else {
+                console.log("V2");
+                setChildWindow(childWindowV2);
+            }
+        });
+    }, [CHILD_WINDOW_HOOK_OPTIONS]);
+    console.log(childWindow);
     return (
         <>
             <h4>Child Window Actions</h4>

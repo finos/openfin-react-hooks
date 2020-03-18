2889cc942bf375d5100d0f5fe8789a233a21e260
Matthew <masmith@scottlogic.co.uk>
2020-03-18 12:56:27 +0000

    Add explicit imports and exports for the OpenFinJavaScriptAPIVersion to prevent compile errors when the Typescript config option 'isolatedModules' is set to true

--- src/useChildWindow.ts
+++ src/useChildWindow.ts
@@ -7,6 +7,7 @@ import createWindow from "./utils/helpers/createWindow";
 import { injectNode, injectNodes } from "./utils/helpers/inject";
 import { isWindowV1, isWindowV2 } from "./utils/helpers/isWindow";
 import reducer, { INITIAL_WINDOW_STATE } from "./utils/reducers/WindowReducer";
+import OpenFinJavaScriptAPIVersion from "./utils/types/enums/OpenFinJavaScriptAPIVersion";
 import WINDOW_ACTION from "./utils/types/enums/WindowAction";
 import WINDOW_STATE from "./utils/types/enums/WindowState";
 
